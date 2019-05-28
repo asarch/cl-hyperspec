1531a7dd1866077633058328d74893656f0cb1ee
asarch <asarch@hotmail.com>
2019-05-28 17:03:51 -0500

    Update the index.htm file path.

--- go.sh
+++ go.sh
@@ -7,3 +7,8 @@
 (unless (ensure-directories-exist #p"hyperspec")
 (unpack-tarball))
 
+(net.aserve:start :port 5477)
+
+(net.aserve:publish-directory :prefix "/hyperspec/" :destination "./share/doc/clisp-hyperspec/")
+
+(format t "Ready!~%Visit: http://localhost:5477/hyperspec/Front/index.htm~%")
